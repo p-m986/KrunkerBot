3308e42aab434cec2ae4e890f41a8c432189047d
ParthMishra0610 <parth.mishra@fourkites.com>
2024-01-14 14:25:00 +0530

    Update 1.1.5 - Revamped flips command

--- cogs/controllers/create_embed.py
+++ cogs/controllers/create_embed.py
@@ -3,6 +3,7 @@ import discord
 class create_embed():
     def __init__(self):
         self.startembed = 0x00FFFF
+        self.resultembed = 0x75ff85
     
     async def createStartEmbed(self, ctx):
         res = discord.Embed(
@@ -25,4 +26,36 @@ class create_embed():
 
         res.set_footer(
             text="PS99Discord", icon_url="https://cdn.discordapp.com/avatars/1184075738496913419/5f2a38f9c2cd28b0696d9716e8e904a1.webp?size=1024")
+        return res
+    
+    async def createFlipresult(self, heads_count, tails_count, author, target, target_choice):
+        if heads_count > tails_count:
+            winner = target if target_choice == "heads" else author
+            author_choice = "tails"
+            file = discord.File("../../Images/heads-removebg.png", filename="image.png")
+        else:
+            winner = target if target_choice == "tails" else author
+            author_choice = "heads"
+            file = discord.File("../../Images/tails-removebg.png", filename="image.png")
+        
+        res = discord.Embed(
+            title=f"COIN FLIP RESULT",
+            description=f"Coin flip between\n{author.mention}: {author_choice}\n{target.mention}: {target_choice}",
+            color = self.resultembed,
+            timestamp = discord.utils.utcnow(),
+        )
+        res.add_field(
+            name = "Coin Flips",
+            value = f"Heads Occoured: {heads_count}\nTails Occoured: {tails_count}",
+        )
+        
+        res.add_field(
+            name = f"Winner is",
+            value = f"{winner.mention}"
+        )
+
+        res.set_thumbnail(url="attachment://image.png")
+
+        res.set_footer(text=f"{discord.utils.utcnow()}")
+
         return res
--- cogs/flip_command.py
+++ cogs/flip_command.py
@@ -2,15 +2,47 @@ from Logs import logevents
 # from database.User import User
 from discord.ext import commands
 from discord.ext.commands import BucketType
-# from cogs.controllers.create_embed import create_embed
+from cogs.controllers.create_embed import createFlipresult
 import discord
 import asyncio
 import random
 
+
+def generate_flip_result(bo):
+    """
+    Returns lists which contain either a 1 or 0 in all its indexes
+    """
+    head_count = []
+    for _ in range(int(bo)):
+        num = random.randint(-9, 10)
+        if num <= 0:
+            head_count += 1
+        else:
+            head_count += 0
+    return head_count, bo - head_count
+
+class Buttons(discord.ui.View):
+    def __init__(self, *, timeout=20):
+        super().__init__(timeout=timeout)
+        self.target_choice = None
+    
+    @discord.ui.button(label = "Heads", style = discord.ButtonStyle.blurple)
+    async def gray_button(self,button:discord.ui.Button,interaction:discord.Interaction):
+        button.style = discord.ButtonStyle.green
+        self.target_choice = "heads"
+        button.disabled = True
+        await interaction.response.edit_message(content = f"Chose Heads", view = self)
+    
+    @discord.ui.button(label = "Tails", style = discord.ButtonStyle.blurple)
+    async def tails_button(self, button: discord.ui.Button, interaction: discord.Interaction):
+        button.style = discord.ButtonStyle.green
+        self.target_choice = "tails"
+        button.disabled = True
+        await interaction.response.edit_message(content = "Chose Tails", view = self)
+
 class Flip(commands.Cog):
     def _init_(self, bot):
         self.bot = bot
-
 
     @commands.cooldown(2, 3, BucketType(2))
     @commands.hybrid_command(name="flip", with_app_command=True)
@@ -27,11 +59,11 @@ class Flip(commands.Cog):
         - Send result embed
         """
         print("Command Recieved") # to be removed
-        if {ctx.channel.id} in [1194652099494035558, 1194653134811832451]:
+        if ctx.channel.id in [1194652099494035558, 1194653134811832451]: # Channel check
             if ctx.author.get_role(1194577286641492069):
-                await ctx.send(f"{ctx.author.mention}Sorry but you are *black Listed* you cant gamble")
+                await ctx.send(f"{ctx.author.mention}Sorry but you are *black Listed* you cant gamble")  # Check if author is blacklisted
             elif target_user.get_role(1194577286641492069):
-                await ctx.send(f"{target_user.mention}Sorry but you are *black Listed* you cant gamble")
+                await ctx.send(f"{target_user.mention}Sorry but you are *black Listed* you cant gamble") # Check if target user is blacklisted
             elif target_user.id == ctx.author.id:
                 await ctx.send("*Sorry but you cant gamble with yourself, Refer to (Exaple)[https://discord.com/channels/1194563432112996362/1194651573297623081/1195671288681873448]*")
             else:
@@ -39,79 +71,18 @@ class Flip(commands.Cog):
                     await ctx.send("**NO PLS NO**\nEven numbers are not allowed in best of this will lead to draws")
 
                 elif (int(bo) < 102 and int(bo) > 0):
-                    waitMessage = await ctx.send(f"{target_user.mention}\n*{ctx.author.name} wants to Gamble with you\nMake sure you know the rules to be followed\nType `heads` or `tails` If you wish to choose heads or tails respectively\n*This will automatically close in 20 Seconds if no correct reply found")
+                    view = Buttons()
+                    await ctx.send(content = f"{target_user.mention}\n**{ctx.author.mention} wants to bet on coinflip with you**\nMake sure you know the rules to be followed\nBest of: {bo}\nChoose `heads` or `tails`\n*This will automatically close in 20 Seconds if no response found*", view = view)
+                    await view.wait()
+                    target_choice = view.target_choice
+                    author_choice = "heads" if target_choice == "tails" else "tails"
 
-                    def check(msg):
-                        return msg.author == target_user and msg.channel == ctx.channel and msg.content.lower() in ["tails", "heads", "head", "tail", "Heads", "Head", "Tail", "Tails"]
+                    heads_count, tails_count = generate_flip_result(bo = bo)
 
-                    try:
-                        msg = await self.bot.wait_for('message', timeout=20.0, check=check)
-                    except asyncio.TimeoutError:
-                        await waitMessage.delete()
-                        await ctx.send(f"{ctx.author.mention}\n{target_user.name} doesn't want to flip")
-                        return
-                    else:
-                        await waitMessage.delete()
-                        if msg.content.lower() in ["heads", "head"]:
-                            user_choice = "tail"
-                        elif msg.content.lower() in ["tails", "tail"]:
-                            user_choice = "head"
+                    resultEmbed = createFlipresult(heads_count = heads_count, tails_count = tails_count, author = ctx.author, target = target_user, target_choice = target_choice)
 
-                        OrignalMessage = await ctx.send(f"{ctx.author.mention} and {target_user.mention} \n Flip starts in few seconds")
-                        await asyncio.sleep(1.5)
-                        
-                        for i in range(int(bo)):
-                            num = random.randint(-1, 2)
-                            if num <= 0:
-                                he_ += 1
-                                value_head += 1
-                            else:
-                                ta_ += 1
-                                value_tail += 1
-                        descriptor = discord.Embed(
-                            title=f"RESULT",
-                            description=f"H: {he_}\tT: {ta_}\n------------------",color=0x11806A)
-                        
-                        if user_choice in ["heads", "head", "h"] and he_ > ta_:
-                            descriptor.add_field(name=f"Winner is ", value=f"{ctx.author.mention}", inline=False)
-                            descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Heads and {target_user.mention} tails",inline=False)
-                            try:
-                                await target_user.send(f"You lost flip which took place with {ctx.author.name}")
-                            except:
-                                print("DMS Error")
-                            try:
-                                await ctx.author.send(f"You Won flip which took place with {target_user.name}")
-                            except:
-                                print("DMS Error")
+                    ctx.send(embed = resultEmbed)
 
-                        elif user_choice in ["tails", "tail", "t"] and ta_ > he_:
-                            descriptor.add_field(name=f"Winner is ",value=f"{ctx.author.mention}", inline=False)
-                            descriptor.add_field(name=f"----------------\n", value=f"{ctx.author.mention} Tails and {target_user.mention} Heads", inline=False)
-                            try:
-                                await target_user.send(f"You lost flip which took place with {ctx.author.name}")
-                            except:
-                                print("DMS Error")
-                            try:
-                                await ctx.author.send(f"You Won flip which took place with {target_user.name}")
-                            except:
-                                print("DMS Error")
-
-                        else:
-                            descriptor.add_field(name=f"Winner is ",value=f"{target_user.mention}",inline=False)
-                            if user_choice in ["tails", "tail", "t"]:
-                                descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Tails and {target_user.mention} Heads",inline=False)
-                            if user_choice in ["heads", "head", "h"]:
-                                descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Heads and {target_user.mention} tails",inline=False)
-                            try:
-                                await target_user.send(f"You Won flip which took place with {ctx.author.name}")
-                            except:
-                                print("DMS Error")
-                            try:
-                                await ctx.author.send(f"You Lost flip which took place with {target_user.name}")
-                            except:
-                                print("DMS Error")
-                        await OrignalMessage.delete()
-                        await ctx.send(embed=descriptor)
                 else:
                     await ctx.send("*Max* best of is 101 and *Min* is 1\n Why do you think its gonna be a zero")
         else:
@@ -126,3 +97,83 @@ class Flip(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(Flip(bot))
+
+
+
+
+# waitMessage = await ctx.reply(f"{target_user.mention}\n**{ctx.author.name} wants to Gamble with you**\nMake sure you know the rules to be followed\nType `heads` or `tails` If you wish to choose heads or tails respectively\n*This will automatically close in 20 Seconds if no correct reply found*")
+#                     print("Waiting...")
+#                     msg = ''
+#                     def check():
+#                         return msg.author == target_user and msg.channel == ctx.channel and msg.content.lower() in ["tails", "heads", "head", "tail", "Heads", "Head", "Tail", "Tails"]
+
+#                     try:
+#                         print("Reached here...")
+#                         msg = await self.bot.wait_for('message', timeout=20.0, check=check)
+#                         print(msg.author)
+#                     except asyncio.TimeoutError:
+#                         await waitMessage.delete()
+#                         await ctx.send(f"{ctx.author.mention}\n{target_user.name} doesn't want to flip")
+#                         return
+#                     else:
+#                         await waitMessage.delete()
+#                         if msg.content.lower() in ["heads", "head"]:
+#                             user_choice = "tail"
+#                         elif msg.content.lower() in ["tails", "tail"]:
+#                             user_choice = "head"
+
+#                         OrignalMessage = await ctx.send(f"{ctx.author.mention} and {target_user.mention} \n Flip starts in few seconds")
+#                         await asyncio.sleep(1.5)
+                        
+#                         for i in range(int(bo)):
+#                             num = random.randint(-1, 2)
+#                             if num <= 0:
+#                                 he_ += 1
+#                                 value_head += 1
+#                             else:
+#                                 ta_ += 1
+#                                 value_tail += 1
+#                         descriptor = discord.Embed(
+#                             title=f"RESULT",
+#                             description=f"H: {he_}\tT: {ta_}\n------------------",color=0x11806A)
+                        
+#                         if user_choice in ["heads", "head", "h"] and he_ > ta_:
+#                             descriptor.add_field(name=f"Winner is ", value=f"{ctx.author.mention}", inline=False)
+#                             descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Heads and {target_user.mention} tails",inline=False)
+#                             try:
+#                                 await target_user.send(f"You lost flip which took place with {ctx.author.name}")
+#                             except:
+#                                 print("DMS Error")
+#                             try:
+#                                 await ctx.author.send(f"You Won flip which took place with {target_user.name}")
+#                             except:
+#                                 print("DMS Error")
+
+#                         elif user_choice in ["tails", "tail", "t"] and ta_ > he_:
+#                             descriptor.add_field(name=f"Winner is ",value=f"{ctx.author.mention}", inline=False)
+#                             descriptor.add_field(name=f"----------------\n", value=f"{ctx.author.mention} Tails and {target_user.mention} Heads", inline=False)
+#                             try:
+#                                 await target_user.send(f"You lost flip which took place with {ctx.author.name}")
+#                             except:
+#                                 print("DMS Error")
+#                             try:
+#                                 await ctx.author.send(f"You Won flip which took place with {target_user.name}")
+#                             except:
+#                                 print("DMS Error")
+
+#                         else:
+#                             descriptor.add_field(name=f"Winner is ",value=f"{target_user.mention}",inline=False)
+#                             if user_choice in ["tails", "tail", "t"]:
+#                                 descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Tails and {target_user.mention} Heads",inline=False)
+#                             if user_choice in ["heads", "head", "h"]:
+#                                 descriptor.add_field(name=f"----------------\n",value=f"{ctx.author.mention} Heads and {target_user.mention} tails",inline=False)
+#                             try:
+#                                 await target_user.send(f"You Won flip which took place with {ctx.author.name}")
+#                             except:
+#                                 print("DMS Error")
+#                             try:
+#                                 await ctx.author.send(f"You Lost flip which took place with {target_user.name}")
+#                             except:
+#                                 print("DMS Error")
+#                         await OrignalMessage.delete()
+#                         await ctx.send(embed=descriptor)
